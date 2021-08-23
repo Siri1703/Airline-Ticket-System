430629a66924287bdf5e385a50da1839940d63a9
siri lakshmi <88713894+Siri1703@users.noreply.github.com>
2021-08-23 19:10:28 +0530

    Add files via upload

---
+++ uservalidation.js
@@ -0,0 +1,9 @@
+function validateuser()
+{
+ var uname=document.getElementById("username");
+ var pass=document.getElementById("password");
+ if(uname!="" && pass!="")
+ {
+     alert("login successful");
+ }
+}
